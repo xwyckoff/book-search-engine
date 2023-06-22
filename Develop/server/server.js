56ba0a0e00a2c45c87b82f96fb5653afc8b3c87a
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-06-21 21:12:10 -0400

    added type definitions

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
