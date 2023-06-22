8e60d3d56439d5a1c2e77a032a4c67e5540836fd
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-06-21 21:56:01 -0400

    added type definitions

---
+++ Develop/server/schema/typeDefs.js
@@ -0,0 +1,33 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Book {
+    authors: [String],
+    description: String!,
+    bookId: String!,
+    image: String,
+    link: String,
+    title: String!
+},
+
+type User {
+    _id: ID!,
+    username: String!,
+    email: String!,
+    password: String!,
+    savedBooks: [Book]
+},
+
+type Auth {
+    token: ID!,
+    user: User
+},
+
+type Query {
+    user(userId: ID!): User
+},
+
+type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth,
+    login(username: String, password: String!): Auth
+}`
