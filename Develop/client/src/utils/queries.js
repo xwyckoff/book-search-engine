5ea59883b1e4005eba7757fdae93de5fa0c178af
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-06-25 16:35:16 -0400

    added apollo client and created client side queries

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client'
+
+export const FIND_SINGLE_USER = gql`
+    query singleUser($userId: ID!) {
+        user(userId: $userId) {
+            _id
+            username
+            email
+            savedBooks
+        }
+    }
+` 
